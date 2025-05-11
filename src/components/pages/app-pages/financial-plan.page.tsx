c04f3e28d2db84a7abd9542ed3938765d92012e5
Gugu P Mokwena <gugu.codes@gmail.com>
2025-05-11 19:50:31 +0200

    investment table

--- src/components/pages/app-pages/financial-plan.page.tsx
+++ src/components/pages/app-pages/financial-plan.page.tsx
@@ -1,13 +1,33 @@
-import { Heading } from "@project/components";
-import { PageLayout } from "@project/components/layouts";
+import { Heading, Table } from "@project/components";
+import { Container, PageLayout } from "@project/components/layouts";
 import { useDocumentTitle } from "@project/hooks";
+import { createColumnHelper } from "@tanstack/react-table";
+import { useUserStore } from "@project/store";
+import { UserApi } from "@project/services";
 
 export const FinancialPlanPage = () => {
   const pageTitle = "My Financial Plans";
   useDocumentTitle(pageTitle);
+
+  const columnHelper = createColumnHelper<UserApi>();
+  const columns = [
+    columnHelper.accessor("user_id", {
+      header: "ID",
+    }),
+    columnHelper.accessor("first_name", {
+      header: "Plan Name",
+    }),
+    columnHelper.accessor("last_name", {
+      header: "Return Estimate",
+    }),
+  ];
+
   return (
     <PageLayout>
       <Heading heading={pageTitle} />
+      <Container>
+        <Table data={[]} columns={columns} />
+      </Container>
     </PageLayout>
   );
 };
