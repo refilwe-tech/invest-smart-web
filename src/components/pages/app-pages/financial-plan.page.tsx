9d69ea506aec185f005f3dfb7404666e03786439
Gugu P Mokwena <gugu.codes@gmail.com>
2025-05-13 13:55:19 +0200

    Plans

--- src/components/pages/app-pages/financial-plan.page.tsx
+++ src/components/pages/app-pages/financial-plan.page.tsx
@@ -2,23 +2,32 @@ import { Heading, Table } from "@project/components";
 import { Container, PageLayout } from "@project/components/layouts";
 import { useDocumentTitle } from "@project/hooks";
 import { createColumnHelper } from "@tanstack/react-table";
-import { useUserStore } from "@project/store";
-import { UserApi } from "@project/services";
+import { investmentService, UserApi } from "@project/services";
+import { useQuery } from "@tanstack/react-query";
+import dayjs from "dayjs";
 
 export const FinancialPlanPage = () => {
   const pageTitle = "My Financial Plans";
   useDocumentTitle(pageTitle);
+  const {data, isLoading} = useQuery({
+    queryKey: ["user-plans"],
+    queryFn: () => investmentService.getUserPlans(),
+    retry: false,
+  })
+
+  console.log("data", data);
 
   const columnHelper = createColumnHelper<UserApi>();
   const columns = [
-    columnHelper.accessor("user_id", {
+    columnHelper.accessor("plan_id", {
       header: "ID",
     }),
-    columnHelper.accessor("first_name", {
+    columnHelper.accessor("plan_name", {
       header: "Plan Name",
     }),
-    columnHelper.accessor("last_name", {
-      header: "Return Estimate",
+    columnHelper.accessor("created_at", {
+      header: "Created At",
+      cell:(info) => dayjs(info.getValue()).format("YYYY-MM-DD HH:mm:ss"),
     }),
   ];
 
@@ -26,7 +35,7 @@ export const FinancialPlanPage = () => {
     <PageLayout>
       <Heading heading={pageTitle} />
       <Container>
-        <Table data={[]} columns={columns} />
+        <Table data={data?.plans ?? []} columns={columns} />
       </Container>
     </PageLayout>
   );
